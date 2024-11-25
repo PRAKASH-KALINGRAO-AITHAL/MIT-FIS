0518365a03fee91e86a72f043c2f8a4409a2b0ac
PRAKASH-KALINGRAO-AITHAL <prakash.aithal@manipal.edu>
2024-11-25 22:26:49 +0530

    PKA

---
+++ PKA/lab_three/lab_three/src/test/java/com/fis/lab_three/LabThreeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fis.lab_three;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabThreeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
